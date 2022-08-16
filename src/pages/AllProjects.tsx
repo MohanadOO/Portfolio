5a9f1f07c6b12ee7b8595d016aabc2ec83436298
MohanadOO <moohand220@gmail.com>
2022-08-16 16:03:07 +0300

    Show an In Progress Page for AllProjects.tsx, Blog.tsx 🔨

--- src/pages/AllProjects.tsx
+++ src/pages/AllProjects.tsx
@@ -1,5 +1,45 @@
-import React from 'react'
+import ProjectCard from '../components/ProjectCard'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
+import { IoTimerOutline } from 'react-icons/io5'
 
 export default function AllProjects() {
-  return <div>AllProjects</div>
+  return (
+    <motion.section
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{ duration: 0.3 }}
+      id='all-projects'
+      className='md:mx-10 lg:mx-20 xl:mx-32 2xl:mx-40 px-10 mx-auto min-h-screen flex flex-col md:flex-row items-center justify-around py-24'
+    >
+      <div className='flex flex-col items-center gap-5 xl:gap-10 max-w-xs xl:max-w-md'>
+        <h1 className='text-4xl lg:text-5xl xl:text-6xl text-center md:text-left text-primary-400'>
+          Page is not available yet <IoTimerOutline className='inline' />
+        </h1>
+        <button className='md:self-start py-2 px-5 rounded-md border border-primary-400 text-primary-400 hover:bg-primary-400 hover:text-primary-white transition-colors xl:text-lg font-pattaya'>
+          <Link to='/'>Back to Home Page</Link>
+        </button>
+      </div>
+      <div>
+        <img
+          className='max-w-xs xl:max-w-sm'
+          src='/images/InProgress.png'
+          alt='Not Found Illustration'
+        />
+      </div>
+    </motion.section>
+  )
+}
+
+const allProjectsVariant = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.1,
+      staggerChildren: 0.4,
+    },
+  },
 }
