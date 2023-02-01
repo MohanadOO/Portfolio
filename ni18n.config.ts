182d358906a87e006b9203525072d1db153f2939
MohanadOO <moohand220@gmail.com>
2023-02-01 14:43:04 +0300

    fix: :bug: vercel not loading translation because of vercel/nft

--- ni18n.config.ts
+++ ni18n.config.ts
@@ -1,7 +1,22 @@
-import type { Ni18nOptions } from 'ni18n'
-const { i18n } = require('./next.config')
+import type { NamespacesNeeded, Ni18nOptions } from 'ni18n'
+import { i18n } from './next.config'
+import { loadTranslations as ni18nLoadTranslations } from 'ni18n'
+import path from 'path'
 
 export const ni18nConfig: Ni18nOptions = {
-  supportedLngs: [...i18n.locales],
+  supportedLngs: i18n?.locales,
   ns: ['home', 'projects', 'blog', 'common'],
+}
+
+export const loadTranslations = async (
+  initialLocale?: string | undefined,
+  namespacesNeeded?: NamespacesNeeded | undefined
+) => {
+  const locales = path.resolve('./', './public/locales')
+
+  return await ni18nLoadTranslations(
+    ni18nConfig,
+    initialLocale,
+    namespacesNeeded
+  )
 }
