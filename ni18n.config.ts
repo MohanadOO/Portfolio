17821cb6fc31e77cc3b35609d1dba6c4e077d133
MohanadOO <moohand220@gmail.com>
2023-02-01 19:28:23 +0300

    feat: :zap: cache translations

--- ni18n.config.ts
+++ ni18n.config.ts
@@ -3,9 +3,29 @@ import { i18n } from './next.config'
 import { loadTranslations as ni18nLoadTranslations } from 'ni18n'
 import path from 'path'
 
+import HttpBackend from 'i18next-http-backend'
+import ChainedBackend from 'i18next-chained-backend'
+import LocalStorageBackend from 'i18next-localstorage-backend'
+
+const isBrowser = typeof window !== 'undefined'
+
 export const ni18nConfig: Ni18nOptions = {
   supportedLngs: i18n?.locales,
   ns: ['home', 'projects', 'blog', 'common'],
+  use: isBrowser ? [ChainedBackend] : undefined,
+  backend: isBrowser
+    ? {
+        backends: [LocalStorageBackend, HttpBackend],
+        backendOptions: [
+          {
+            expirationTime: 24 * 60 * 60 * 1000,
+          },
+          {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+          },
+        ],
+      }
+    : undefined,
 }
 
 export const loadTranslations = async (
