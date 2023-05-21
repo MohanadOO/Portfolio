275627d3e65b06e9a05fa42372a60eb8f0acde82
MohanadOO <moohand220@gmail.com>
2023-05-21 22:10:01 +0300

    fix: 🐛 elements direction shift after mount.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 
-export default function Document() {
+export default function Document(props: DocumentProps) {
   return (
-    <Html className='scroll-smooth bg-primary-white text-primary-black'>
+    <Html
+      className='scroll-smooth bg-primary-white text-primary-black'
+      lang={props.locale === 'ar' ? 'ar' : 'en'}
+      dir={props.locale === 'ar' ? 'rtl' : 'ltr'}
+    >
       <Head />
       <body className='dark:bg-primary-dark dark:text-primary-white'>
         <Main />
